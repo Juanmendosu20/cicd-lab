b2e31dbb117226d443671ec54a5cf127b5d07a75
Juanmendosu20 <159053766+Juanmendosu20@users.noreply.github.com>
2026-04-23 21:57:00 +0000

    feat: mensaje actualizado a Hola Arquitectos

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function sumar(a, b) {
-  return a + b;  // CORREGIDO
+  return a + b;
 }
 
 if (require.main === module) {
@@ -8,7 +8,7 @@ if (require.main === module) {
 
   const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hola DevOps!\n');
+    res.end('Hola Arquitectos!\n');
   });
 
   server.listen(PORT, () => {
